cf7cd423e879ea46029e4240ae11bbbf3bd76348
ben <grzybowski1911@gmail.com>
2022-09-05 16:05:59 -0700

    change bar graph to pie graph & update datasets

--- public/js/app.js
+++ public/js/app.js
@@ -30,6 +30,7 @@ if(document.getElementById('myChart')) {
         let liftNames = [];
         let liftDates = [];
         let totalWeight = [];
+        let allLiftCats = [];
         jsonLiftData.forEach(element => {
             //console.log(element);
             let regex = /[0-9]{4}-[0-9]{2}-[0-9]{2}/i;
@@ -37,29 +38,100 @@ if(document.getElementById('myChart')) {
             liftNames.push(element.lift_name);
             //console.log( Number(element.weight) * Number(element.reps) * Number(element.sets) );
             totalWeight.push(Number(element.weight) * Number(element.reps) * Number(element.sets));
+            allLiftCats.push(element.category);
         });
-        //console.log(totalWeight);
+
+        // use new set to remove duplicates in an array & turn back into array after - not using atm 
+        // let singleCats = [...new Set(liftCat)];
+
+        let categories = ['chest', 'arms', 'back', 'core', 'legs', ''];
+        let iter = 0; 
+        let count = 0;
+        let chest = 0;
+        let arms = 0;
+        let back = 0;
+        let core = 0;
+        let legs = 0; 
+        let uncategorized = 0;
+        for(cat of allLiftCats) {
+            //if(cat == categories[iter]) {
+            //    count++;
+            //}
+            if(cat == 'chest') {
+                chest++;
+            }
+            if(cat == 'arms') {
+                arms++;
+            }
+            if(cat == 'back') {
+                back++;
+            }
+            if(cat == 'core') {
+                core++;
+            }
+            if(cat == 'legs') {
+                legs++;
+            }
+            if(cat == '') {
+                uncategorized++;
+            }
+        }
+        let totals = [];
+        totals.push(chest);
+        totals.push(arms);
+        totals.push(back);
+        totals.push(core);
+        totals.push(legs);
+        totals.push(uncategorized);
+
+        console.log(totals);
 
         const data = {
-            labels: liftNames,
+            labels: categories,
             datasets: [{
-              label: 'Weight Moved',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: totalWeight,
+              label: 'Lift Category',
+              data: totals,
+              backgroundColor: [
+                '#F2EBDF',
+                '#BFAC95',
+                '#59281D',
+                '#400909',
+                '#A64B4B'
+              ],
+              hoverOffset: 4
             }]
           };
-        
+
           const config = {
-            type: 'line',
+            type: 'doughnut',
             data: data,
-            options: {}
-          };
+        };
+
+        const myChart = new Chart(
+          document.getElementById('myChart'),
+          config
+        );
+
+        //const data = {
+        //    labels: categoriess,
+        //    datasets: [{
+        //      label: 'Weight Moved',
+        //      backgroundColor: 'rgb(255, 99, 132)',
+        //      borderColor: 'rgb(255, 99, 132)',
+        //      data: totalWeight,
+        //    }]
+        //};
         
-          const myChart = new Chart(
-            document.getElementById('myChart'),
-            config
-          );
+          //const config = {
+          //  type: 'line',
+          //  data: data,
+          //  options: {}
+          //};
+        
+          //const myChart = new Chart(
+          //  document.getElementById('myChart'),
+          //  config
+          //);
     };
     // Send request for data to "api" route
     dateReq.open("get", "api/lift-data", true);
